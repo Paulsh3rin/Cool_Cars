a33b025784befdb2e431fdb4f05e0d9762bab6c8
Sherin Paul <114738504+Paulsh3rin@users.noreply.github.com>
2023-09-01 03:20:38 +0000

    Attempt to scrape from multiple pages

--- Cars(00-05).py
+++ Cars(00-05).py
@@ -1,13 +1,34 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+ 
+cars_url = "https://www.kbb.com/car-finder/page-{}/"
 
-cars_url = "https://www.kbb.com/car-finder/?manufacturers=bmw&years=2000-2005&seolink=false"
+car_names = []
 
-response = requests.get(cars_url)
-soup = BeautifulSoup(response.content, "html.parser")
+#iterate through pages
+for page_num in range(1, 57):
+    page_url = cars_url.format(page_num)  # Generate the URL for the current page
+    
+    # Additional parameters
+    parameters = {
+        "manufacturers": "bmw|audi|bentley|astonmartin|acura|alfaromeo|volvo|volkswagen|toyota|vinfast|tesla|suzuki|srt|subaru|smart|scion|saturn|saab|rollsroyce|rivian|ram|porsche|polestar|pontiac|plymouth|panoz|nissan|mini|mitsubishi|oldsmobile|buick|cadillac|chevrolet|chrysler|daihatsu|eagle|fiat|ford|genesis|gmc|hummer|infiniti|jaguar|kia|landrover|lincoln|lucid|maybach|mclaren|mercury|mercedesbenz|mazda|maserati|lotus|lexus|lamborghini|jeep|isuzu|hyundai|honda|geo|freightliner|fisker|dodge|ferrari|daewoo",
+        "years": "2000-2005",
+        "seolink": "false"
+    }
 
-car_names = [car.text.strip() for car in soup.find_all('h2')]
+    response = requests.get(page_url, params=parameters)
+    soup = BeautifulSoup(response.content, "html.parser")
 
+#Extract car names from current page and append to list 
+    car_names.extend([car.text.strip() for car in soup.find_all('h2')])
+
+    time.sleep(5)
+
+#Create the dataframe
 df = pd.DataFrame({'Model':car_names})
 print(df)
+
+
+
+
